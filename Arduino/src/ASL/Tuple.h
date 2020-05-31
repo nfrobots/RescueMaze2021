edd8d2abc738fee698f839ff4e52502b5d00dfa0
FelixWollenhaupt <fan.wollenhaupt@gmail.com>
2020-05-31 12:04:08 +0200

    finally made tuples usable lol

--- Arduino/src/ASL/Tuple.h
+++ Arduino/src/ASL/Tuple.h
@@ -1,4 +1,7 @@
+#pragma once
+
 #include "types.h"
+#include "Util.h"
 
 template<asl::size_t Index, typename T>
 class _Tuple_elem
@@ -13,7 +16,7 @@ private:
 template<asl::size_t Index, typename ... Dummy> class _Rec_tuple_elem_gen {}; //base case
 
 template<asl::size_t Index, typename T, typename ... Us>
-class _Rec_tuple_elem_gen<Index, T, Us...> : public _Tuple_elem<Index, T>, public _Rec_tuple_elem_gen<Index + 1, Us...> 
+class _Rec_tuple_elem_gen<Index, T, Us...> : public _Tuple_elem<Index, T>, public _Rec_tuple_elem_gen<Index + 1, Us...>
 {
 public:
 	_Rec_tuple_elem_gen(const T& arg, const Us& ... args) : _Tuple_elem<Index, T>(arg), _Rec_tuple_elem_gen<Index + 1, Us...>(args...) {}
@@ -35,17 +38,27 @@ struct _Get_type
 template<typename T, typename ... Us>
 struct _Get_type<0, T, Us...>
 {
-	using type =  T;
+	using type = T;
 };
 
 template<asl::size_t Index, typename ... Ts>
-auto& get(Tuple<Ts...> tuple)
+auto& get(Tuple<Ts...>& tuple)
 {
-	return tuple._Tuple_elem<Index, _Get_type<Index, Ts...>::type>::get();
+	return tuple._Tuple_elem<Index, typename _Get_type<Index, Ts...>::type>::get();
 }
 
 template<typename ... Ts>
-asl::size_t get_size(Tuple<Ts...>)
+asl::size_t get_size(const Tuple<Ts...>&)
 {
 	return sizeof...(Ts);
+}
+
+template<asl::size_t Index = 0, typename C, typename ... Ts>
+typename enable_if<Index >= sizeof...(Ts), void>::type apply(const C& callable, Tuple<Ts...>& tuple) {} //base case
+
+template<asl::size_t Index = 0, typename C, typename ... Ts>
+typename enable_if<Index < sizeof...(Ts), void>::type apply(const C& callable, Tuple<Ts...>& tuple)
+{
+	callable.operator()(get<Index>(tuple));
+	apply<Index + 1, C, Ts...>(callable, tuple);
 }
---
+++ Arduino/src/ASL/Util.h
@@ -0,0 +1,7 @@
+#pragma once
+
+template<bool B, class T = void>
+struct enable_if {};
+
+template<class T>
+struct enable_if<true, T> { typedef T type; };
