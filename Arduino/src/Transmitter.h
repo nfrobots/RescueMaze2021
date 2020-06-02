253aa6a13e8efacc79c2d28c2504c2968312db4e
FelixWollenhaupt <fan.wollenhaupt@gmail.com>
2020-06-02 18:19:27 +0200

    changed initialization; documented code

--- Arduino/src/Transmitter.h
+++ Arduino/src/Transmitter.h
@@ -5,6 +5,14 @@
 #include <Arduino.h>
 
 
+/**
+ * Class holding a reference to a value which is to be transmitted and a name for the value.
+ * 
+ * Param:
+ *  name: name of the value
+ *  ref: value to be transmitted.
+ */
+
 template<typename T>
 class TrValue
 {
@@ -15,41 +23,73 @@ public:
 	T& ref;
 };
 
+/**
+ * Parser object used to transmitt TrValue.
+ * 
+ * Param:
+ *  callable: actual parser callable class (lambda!) which takes a name as String and value as any type. It should return a String.
+ *  interface: Class with .print() function of type returned by C such as Serial.
+ *  head: callable class (lambda!) which is executed at the beginning of each transmittion
+ *  foot: callable class (lambda!) which is executed at the end of eacht transmittion
+ */
 
-template<typename I, typename C>
+template<typename C, typename I, typename H, typename F>
 class TrParser
 {
 public:
-	TrParser(I& interface, C callable, String head, String foot)
+	TrParser(C callable, I& interface, H head, F foot)
      : callable(callable), interface(interface), head(head), foot(foot) {}
 
 	template<typename T>
-	void operator()(const TrValue<T>& trvalue)
+	void operator()(TrValue<T>& trvalue) const
 	{
 		interface.print(callable(trvalue.name, trvalue.ref));
 	}
 
 	C callable;
     I& interface;
-    String head, foot;
+    H head;
+    F foot;
 };
 
-template<typename P, typename I, typename ... Ts>
+/**
+ * Default Json parser
+ */
+TrParser JSON_TR_PARSER_DEFAULT(
+    [](auto name, auto value) {
+        return "\t\"" + name + "\": " + value + ",\n";
+    },
+    Serial,
+    [](){ return String("{\n"); },
+    [](){ return String("\t\"time\": " + String(millis()) + "\n}\n"); }
+);
+
+/**
+ * JsonTransmitter object used to transmitt data sucha as sensor values in (json) format.
+ * Format can be customized.
+ * 
+ * Param:
+ *  parser: TrParser such as JSON_TR_PARSER_DEFAULT
+ *  args...: arbitrary amount of TrValues to be transmittted
+ */
+
+template<template<typename, typename, typename, typename> typename P,
+    typename C, typename I, typename H, typename F, typename ... Ts>
 class JsonTransmitter
 {
 public:
-	JsonTransmitter(I& interface, P parser, const Ts& ... args) 
-        : parser(parser), interface(interface), attrb(args...) {}
+	JsonTransmitter(P<C, I, H, F> parser, const Ts& ... args) 
+        : parser(parser), interface(parser.interface), attrb(args...) {}
 
 	void transmitt()
 	{
-        interface.print(parser.head);
+        interface.print(parser.head());
 		apply(parser, attrb);
-        interface.print(parser.foot);
+        interface.print(parser.foot());
 	}
 
-public:
-	P parser;
+private:
+	P<C, I, H, F> parser;
     I& interface;
 	Tuple<Ts...> attrb;
 };
