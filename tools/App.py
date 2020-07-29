5e68ada004a54f55f3e362d2406285cf76c4fe9d
FelixWollenhaupt <fan.wollenhaupt@gmail.com>
2020-07-29 13:14:44 +0200

    beginning SensorViewVisaulMode impl

--- tools/App.py
+++ tools/App.py
@@ -1,8 +1,13 @@
-from tools import mapDrawer
+try:
+    import mapDrawer
+except ImportError:
+    from tools import mapDrawer
 
 from tkinter import Tk, Frame, Canvas, Button, Label
 from pathlib import Path
 import json
+
+UPDATE_RATE = 500
 
 BUTTON_CONFIG = {
     'bd': 1,
@@ -14,7 +19,8 @@ class App(Tk):
     def __init__(self):
         super().__init__()
 
-        self.state("zoomed")
+        self.geometry("1920x1080")
+        self.update_idletasks()
         self.title("Roboti App")
         self.grid_rowconfigure(0, weight=3) # make module frame expand
         self.grid_columnconfigure(1, weight=3)
@@ -38,9 +44,9 @@ class App(Tk):
             self.modules[Module].grid(row=0, column=1, sticky='nswe')
 
         self.enable_module(Home)
-        
+
         self.update()
-        self.after(1000, self.update)
+        self.after(UPDATE_RATE, self.update)
 
     def enable_module(self, module):
         self.modules[module].tkraise()
@@ -48,7 +54,7 @@ class App(Tk):
 
     def update(self):
         self.active_module.update()
-        self.after(1000, self.update)
+        self.after(UPDATE_RATE, self.update)
 
 
 class Home(Frame):
@@ -58,8 +64,8 @@ class Home(Frame):
         self.text.pack()
 
     def update(self):
-        print("update")
         self.text.configure(text="w: {}; h: {}".format(self.master.winfo_width(), self.master.winfo_height()))
+
 
 class MapVisualisation2(Frame):
     def __init__(self, root):
@@ -73,6 +79,7 @@ class MapVisualisation2(Frame):
     def update(self):
         pass
 
+
 class SensorView(Frame):
     def __init__(self, root):
         super().__init__(root)
@@ -80,7 +87,7 @@ class SensorView(Frame):
         self.grid_rowconfigure(1, weight=40)
         self.grid_columnconfigure(0, weight=40)
 
-        self.mode_selector_frame = Frame(self, bg="green")
+        self.mode_selector_frame = Frame(self)
         self.mode_selector_frame.grid(row=0, column=0, sticky='nswe')
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
@@ -89,7 +96,7 @@ class SensorView(Frame):
         self.modes = {}
 
         for counter, Mode in enumerate(SENSOR_VIEW_MODES):
-            self.mode_selectors[Mode] = Button(self.mode_selector_frame, text=Mode.__name__, command=lambda Mode=Mode: self.enable_mode(Mode))
+            self.mode_selectors[Mode] = Button(self.mode_selector_frame, text=Mode.__name__, cnf=BUTTON_CONFIG, command=lambda Mode=Mode: self.enable_mode(Mode))
             self.mode_selectors[Mode].grid(row=0, column=counter, sticky='nswe')
             self.mode_selector_frame.grid_columnconfigure(counter, weight=1)
             self.mode_selector_frame.grid_rowconfigure(0, weight=1)
@@ -107,7 +114,6 @@ class SensorView(Frame):
         self.active_mode.update()
 
 
-
 class SersorViewNumberMode(Frame):
     def __init__(self, root):
         super().__init__(root, bg="yellow")
@@ -115,14 +121,36 @@ class SersorViewNumberMode(Frame):
     def update(self):
         pass
 
-class SersorViewNumberMode2(Frame):
+# should have ratio of about 1.4: eg 28 and 20
+N_TILE_WIDHT = 28
+N_TILE_HEIGHT = 20
+
+class SersorViewVisualMode(Frame):
     def __init__(self, root):
-        super().__init__(root, bg="green")
+        super().__init__(root)
+        self.canvas = Canvas(self)
+        self.canvas.grid(row=0, column=0, sticky='nswe')
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.width, self.height, self.grid_tile_size, self.pad = 0, 0, 0, 0
 
     def update(self):
-        pass
+        self.canvas.delete("all")
+        new_width, new_height = self.canvas.winfo_width(), self.canvas.winfo_height()
+        if new_width != self.width or new_height != self.height:
+            self.pad = int(min(new_width, new_height) / 100)
+            self.grid_tile_size = int(min((new_height - (2 * self.pad)) / N_TILE_HEIGHT, (new_width - (2 * self.pad)) / N_TILE_WIDHT))
+            
+        for i in range(N_TILE_WIDHT):
+            for j in range(N_TILE_HEIGHT):
+                self.canvas.create_rectangle(self.coord_helper(i, j, i + 1, j + 1), outline="#aaa")
 
-SENSOR_VIEW_MODES = (SersorViewNumberMode,SersorViewNumberMode2)
+        self.canvas.create_rectangle(self.coord_helper(1, 2, 2, 8), fill="#555")
+
+    def coord_helper(self, *args):
+        return [arg * self.grid_tile_size + self.pad for arg in args]
+
+SENSOR_VIEW_MODES = (SersorViewVisualMode, SersorViewNumberMode)
 
 MODULES = (Home, MapVisualisation2, SensorView)
 
