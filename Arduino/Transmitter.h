b95f75aadbb8362ef42f3a8214a634d91ed7e456
FelixWollenhaupt <fan.wollenhaupt@gmail.com>
2020-08-10 15:30:49 +0200

    removed redundand template argument; imporved doc

--- Arduino/Transmitter.h
+++ Arduino/Transmitter.h
@@ -27,17 +27,17 @@ public:
  * Parser object used to transmitt TrValue.
  * 
  * Param:
- *  callable: actual parser callable class (lambda!) which takes a name as String and value as any type. It should return a String.
+ *  callable: actual parser callable class (non mutable lambda!) which takes a name as String and value as any type. It should return a String.
  *  interface: Class with .print() function taking single argument returned by callable. E.g. Serial
- *  head: callable class (lambda!) which is executed at the beginning of each transmittion
- *  foot: callable class (lambda!) which is executed at the end of eacht transmittion
+ *  head: callable class (non mutable lambda!) which is executed at the beginning of each transmittion
+ *  foot: callable class (non mutable lambda!) which is executed at the end of eacht transmittion
  */
 
 template<typename C, typename I, typename H, typename F>
 class TrParser
 {
 public:
-	TrParser(const C& callable, I& interface, const H& head, const F& foot) //can not pass interface as constant reference as print function may not be constant
+	TrParser(const C& callable, I& interface, const H& head = [](){}, const F& foot = [](){}) //can not pass interface as constant reference as print function may not be constant
      : callable(callable), interface(interface), head(head), foot(foot) {}
 
 	template<typename T>
@@ -55,7 +55,7 @@ public:
 /**
  * Default Json parser
  */
-TrParser JSON_TR_PARSER_DEFAULT(
+TrParser JSON_TR_PARSER_DEFAULT (
     [](const auto& name, const auto& value) {
         return "\t\"" + name + "\": " + value + ",\n";
     },
@@ -73,12 +73,11 @@ TrParser JSON_TR_PARSER_DEFAULT(
  *  args...: arbitrary amount of TrValues to be transmittted
  */
 
-template<template<typename, typename, typename, typename> typename P,
-    typename C, typename I, typename H, typename F, typename ... Ts>
-class JsonTransmitter
+template<typename C, typename I, typename H, typename F, typename ... Ts>
+class Transmitter
 {
 public:
-	JsonTransmitter(const P<C, I, H, F>& parser, const Ts& ... args)
+	Transmitter(const TrParser<C, I, H, F>& parser, const Ts& ... args)
         : parser(parser), interface(parser.interface), attrb(args...) {}
 
 	void transmitt()
@@ -89,7 +88,7 @@ public:
 	}
 
 private:
-	const P<C, I, H, F>& parser;
+	const TrParser<C, I, H, F>& parser;
     I& interface;
 	asl::Tuple<Ts...> attrb;
 };
