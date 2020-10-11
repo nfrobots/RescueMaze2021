f784e8d8b17a7efd46d4cf67ced5c8593b59f7d1
FelixWollenhaupt <fan.wollenhaupt@gmail.com>
2020-10-11 15:04:26 +0200

    basic read only client

--- tools/socket_tst_client.py
+++ tools/socket_tst_client.py
@@ -1,11 +1,12 @@
 import socket
 
-HOST = '10.42.0.21'
+# HOST = '10.42.0.21'
+HOST = '192.168.178.48'
 PORT = 1337
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Moin, Meister')
-    data = s.recv(1024)
+    while True:
+        print(s.recv(1024))
 
-print("recieved", repr(data))
+print("closed")
