ae4842fb8d520c8176401ae9d8eab7579a11ce4e
FelixWollenhaupt <fan.wollenhaupt@gmail.com>
2021-01-28 16:08:44 +0100

    can solve floating walls now

--- Pi/Main.py
+++ Pi/Main.py
@@ -1,7 +1,7 @@
 from RMMLIB4 import Mapping, Logger, Constants
 from RMMLIB4.Constants import Direction, RelDirection
 
-Logger.PATH = './Pi/out/log.txt'
+Logger.I_PATH = './Pi/out/log.txt'
 Logger.clear()
 
 #import Pi.Interpreter
@@ -10,28 +10,44 @@ import tools.Simulation
 Interpreter = tools.Simulation
 Mover = tools.Simulation
 
-map = Mapping.Map(logging=True)
+UNKNOWN_FILTER = Mapping.create_any_tile()
+UNKNOWN_FILTER[Constants.KNOWN] = False
+
+map = Mapping.Map(logging=True, path_pre_expand=True)
 just_started = True
 
 while True:
-    # if map.robotIsAtStart() and not just_started: ##### FIX!!! ROOBT MIGHT GO TO START, BUT IS NOT FINISHED
-    #     break
     if just_started:
         just_started = False
 
     current_tile = Interpreter.get_data()
     map.setAtRobot(current_tile)
+    
+    res = map.search(Mapping.Position(map.robot.x, map.robot.y), UNKNOWN_FILTER)
+    print(res[1], res[2], map.robot.x, map.robot.y)
+    if res == (None, None, None):
+        print("DONE")
+        exit()
+    path = map.findPath(map.robot.x, map.robot.y, res[1], res[2])
+    directions = map.pathToDirections(path)
 
-    for relDirection in (RelDirection.LEFT, RelDirection.FORWARD, RelDirection.RIGHT, RelDirection.BACKWARD):
-        if map.getAtRobot()[map.relDirectionToDirection(relDirection)]: #wall
-            continue
-        destination = map.get(map.robot.x + map.relDirectionToOffset(relDirection)[0],
-                              map.robot.y + map.relDirectionToOffset(relDirection)[1])
+    for direction in directions:
+        driveDirection = map.directionToRelDirection(direction)
+
+        if map.getAtRobot()[direction]:
+            break
+
+        destination = map.get(map.robot.x + map.directionToOffset(direction)[0],
+                            map.robot.y + map.directionToOffset(direction)[1])
+
         if destination != None and destination[Constants.BLACK]:
-            continue
-        drive_succes = Mover.driveRobot(relDirection)
+            break
+
+        drive_succes = Mover.driveRobot(driveDirection)
         if drive_succes == True:
-            map.driveRobot(relDirection)
+            map.driveRobot(driveDirection)
             break
         if drive_succes == Constants.BLACK: # no succes due to black tile
-            map.setAttributeAtRobotOffset(relDirection, Constants.BLACK, True)
+            map.setAttributeAtRobotOffset(driveDirection, Constants.BLACK, True)
+            map.setAttributeAtRobotOffset(driveDirection, Constants.KNOWN, True)
+            break
