b6924b4c68429cc93e51bd902c608084acabcefa
FelixWollenhaupt <fan.wollenhaupt@gmail.com>
2021-04-12 14:47:41 +0200

    rewrite in class version

---
+++ Pi/CallibratorCI.py
@@ -0,0 +1,102 @@
+import json
+from enum import Enum
+from pathlib import Path
+
+from Pi.ReceiverCI import Receiver #, ArduinoData 
+
+
+class CalibrationTarget(str, Enum):
+    COLOR_RED         = "COLOR_RED"
+    COLOR_WHITE       = "COLOR_WHITE"
+    COLOR_SILVER      = "COLOR_SILVER"
+    COLOR_BLACK       = "COLOR_BLACK"
+    WALL_LEFT         = "WALL_LEFT"
+    WALL_FRONT        = "WALL_FRONT"
+    WALL_RIGHT        = "WALL_RIGHT"
+    WALL_BACK         = "WALL_BACK"
+    WALL_2ND_FRONT_LD = "WALL_2ND_FRONT_LD"
+    GYRO_FLAT         = "GYRO_FLAT"
+    GYRO_INCLINE      = "GYRO_INCLINE"
+
+
+
+class Calibrator:
+    def __init__(self):
+        self.path = Path(__file__).parent / 'data/data.json'
+        self.calibration_data = {}
+
+    def load_calibration(self):
+        with open(self.path, 'r') as f:
+            self.calibration_data = json.load(f)
+
+    def save_calibration(self):
+        if not self.calibration_data:
+            print("[WARNING] trying to save empty calibration. Aborting...")
+            return
+
+        with open(self.path, 'w') as f:
+            json.dump(self.calibration_data, f)
+        
+    def calibrate(self, target: CalibrationTarget):
+        sensor_data = Receiver().get_data_s()
+        
+        if target in (CalibrationTarget.COLOR_RED, CalibrationTarget.COLOR_WHITE,
+                    CalibrationTarget.COLOR_SILVER, CalibrationTarget.COLOR_BLACK):
+            self.calibration_data[target] = {
+                "RED": sensor_data.color_red,
+                "GRE": sensor_data.color_green,
+                "BLU": sensor_data.color_blue,
+                "ALP": sensor_data.color_alpha
+            }
+        elif target == CalibrationTarget.WALL_LEFT:
+            self.calibration_data[target] = {
+                "IR0": sensor_data.ir_0,
+                "IR1": sensor_data.ir_1,
+                "USL": sensor_data.ultrasonic_left
+            }
+        elif target == CalibrationTarget.WALL_FRONT:
+            self.calibration_data[target] = {
+                "IR2": sensor_data.ir_2,
+                "IR3": sensor_data.ir_3,
+                "LIR": sensor_data.long_distance_ir
+            }
+        elif target == CalibrationTarget.WALL_RIGHT:
+            self.calibration_data[target] = {
+                "IR4": sensor_data.ir_4,
+                "IR5": sensor_data.ir_5,
+                "USR": sensor_data.ultrasonic_right
+            }
+        elif target == CalibrationTarget.WALL_BACK:
+            self.calibration_data[target] = {
+                "IR6": sensor_data.ir_6,
+                "IR7": sensor_data.ir_7
+            }
+        elif target == CalibrationTarget.WALL_2ND_FRONT_LD:
+            self.calibration_data[target] = {
+                "LIR": sensor_data.long_distance_ir
+            }
+        elif target in (CalibrationTarget.GYRO_FLAT, CalibrationTarget.GYRO_INCLINE):
+            self.calibration_data[target] = {
+                "GYX": sensor_data.gyro_x,
+                "GYY": sensor_data.gyro_y,
+                "GYZ": sensor_data.gyro_z
+            }
+        else:
+            print("[ERROR] calibration target is not implemented")
+        
+
+
+
+
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    clb = Calibrator()
+    # [clb.calibrate(a) for a in CalibrationTarget]
+    # pprint(clb.calibration_data)
+    # clb.save_calibration()
+
+    clb.load_calibration()
+    clb.calibrate(CalibrationTarget.COLOR_RED)
+    pprint(clb.calibration_data)
---
+++ Pi/ReceiverCI.py
@@ -0,0 +1,146 @@
+import json
+import time
+import serial
+from dataclasses import dataclass
+
+@dataclass
+class ArduinoData:
+    valid: bool
+    long_distance_ir: int = 0
+    ir_0: int = 0
+    ir_1: int = 0
+    ir_2: int = 0
+    ir_3: int = 0
+    ir_4: int = 0
+    ir_5: int = 0
+    ir_6: int = 0
+    ir_7: int = 0
+    gyro_x: int = 0
+    gyro_y: int = 0
+    gyro_z: int = 0
+    color_red: int = 0
+    color_green: int = 0
+    color_blue: int = 0
+    color_alpha: int = 0
+    greyscale: int = 0
+    ultrasonic_left: int = 0
+    ultrasonic_right: int = 0
+
+
+
+class Receiver:
+    def __new__(cls, port='/dev/ttyACM0'):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Receiver, cls).__new__(cls)
+            cls.instance._init(port)
+        return cls.instance
+
+    def __init__(self, port=None):
+        """Receiver is a singelton.
+
+        Args:
+            port (str, optional): serial device path/port. Defaults to '/dev/ttyACM0'.
+        """
+        if port and port != self.port:
+            print(f"[WARNING] tried to create Receiver on port '{port}'. Port is already set to '{self.port}'")
+
+    def _init(self, port='/dev/ttyACM0'):
+        self.port = port
+        self.REQUEST_BYTE = 'd'.encode('utf-8')
+        self.connected = False
+
+    def connect(self):
+        self.serial_connection = serial.Serial(self.port, 9600, timeout=0.2)
+
+        while self.serial_connection.in_waiting == 0:
+            print("[INFO] waiting for Arduino to finish initialization and establish serial connection")
+            time.sleep(0.5)
+        
+        try:
+            setup_info = self.serial_connection.read(1024).decode('utf-8')
+        except UnicodeDecodeError:
+            print("[ERROR] could not decode setup info")
+            setup_info = "[ERROR]"
+
+        if "[ERROR]" in setup_info:
+            print("[ERROR] connecting to Arduino failed")
+        else:
+            print(f"[OK] serial connection established with info: {setup_info}")
+            self.connected = True
+
+    def request_data_as_bytes(self):
+        """Requests data from arduino and returns it as a bytes or None if error occured"""
+        if not self.connected:
+            print("[ERROR] Receiver is not connected!")
+            return None
+
+        self.serial_connection.write(self.REQUEST_BYTE)
+        valid = True
+        received_data = b''
+        incomeing_char = self.serial_connection.read()
+        
+        while incomeing_char != b'}':
+            if incomeing_char == b'':
+                print("[WARNING] serial data request timed out")
+                valid = False
+                break
+
+            received_data += incomeing_char
+            incomeing_char = self.serial_connection.read()
+        
+        if valid:
+            return received_data + b'}'
+       #else:
+       #    return None
+
+    def get_data(self):
+        """Requests data from arduino and returns it as a ArduinoData dataclass"""
+        data_bytes = self.request_data_as_bytes()
+        if data_bytes == None:
+            return ArduinoData(valid=False)
+        try:
+            data_dict = json.loads(data_bytes)
+        except json.decoder.JSONDecodeError:
+            print("[ERROR] could not decode bytes to json")
+
+        return ArduinoData(
+            valid               = True,
+            long_distance_ir    = data_dict["LIR"],
+            ir_0                = data_dict["IR0"],
+            ir_1                = data_dict["IR1"],
+            ir_2                = data_dict["IR2"],
+            ir_3                = data_dict["IR3"],
+            ir_4                = data_dict["IR4"],
+            ir_5                = data_dict["IR5"],
+            ir_6                = data_dict["IR6"],
+            ir_7                = data_dict["IR7"],
+            gyro_x              = data_dict["GYX"],
+            gyro_y              = data_dict["GYZ"],
+            gyro_z              = data_dict["GYZ"],
+            color_red           = data_dict["RED"],
+            color_green         = data_dict["GRE"],
+            color_blue          = data_dict["BLU"],
+            color_alpha         = data_dict["ALP"],
+            greyscale           = data_dict["GRS"],
+            ultrasonic_left     = data_dict["USL"],
+            ultrasonic_right    = data_dict["USR"]
+        )
+
+    def get_data_s(self):
+        """like get_data, but cannot return invalid"""
+        if not self.connected:
+            self.connect()
+
+        data = self.get_data()
+        while data.valid == False:
+            data = self.get_data()
+        return data
+
+
+if __name__ == "__main__":
+    rcv = Receiver(port='COM3')
+    # rcv.connect()
+    print(rcv.get_data_s())
+
+    rcv2 = Receiver(port='asbs')
+    print(rcv2.connected)
